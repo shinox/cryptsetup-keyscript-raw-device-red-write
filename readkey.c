ccfa3c5fd78ec4905086e3090533004109daf8f0
shinox <daniel.mikolajczyk@kcl.ac.uk>
2016-06-24 11:02:31 +0100

    Update readkey.c

--- readkey.c
+++ readkey.c
@@ -5,19 +5,45 @@
 void main(int argc, char *argv[])
 {
 	int blockSize = 512;
-	int keySize = 2048;
+	int keySize = 2048;	
 
-	if (  argc == 2 
+	FILE *device;	    
+
+	if (  argc == 3 
            && (sizeof(argv[1]) / sizeof(char)) > 1
-	   && (atoi(argv[1]) % 512) == 0
-	   )
-		keySize = atoi(argv[1]);
+           && (sizeof(argv[2]) / sizeof(char)) > 1
+	   && (atoi(argv[2]) % 512) == 0
+	   ) {
+	   	device = fopen(argv[1], "r");
+		if(device == NULL) { 
+			printf("\nI got trouble opening the device %s\n", argv[1]);
+			exit(EXIT_FAILURE);
+		}
+		keySize = atoi(argv[2]);		
+	}
+	else if (  argc == 2 
+	        && (sizeof(argv[1]) / sizeof(char)) > 1
+		) {
+	  	device = fopen(argv[1], "r");
+		if(device == NULL) { 
+			printf("\nI got trouble opening the device %s\n", argv[1]);
+			exit(EXIT_FAILURE);
+		}
+
+	}
+	else {
+		
+		printf("\nUsage: \n");
+		printf("\nKey Size Provided: \n");
+		printf("\n\t\treadkey </path/to/device> <keysize> \n");
+		printf("\nDefault key size: %d\n", keySize);
+		printf("\n\t\treadkey </path/to/device>\n");
+		exit(1);
+	}
 
 	int count;
 
 	char *block;
-
-	FILE *f;
 
 	/* Verify if key is multiple of blocks */
 	int numBlocks = 0;
@@ -25,19 +51,15 @@ void main(int argc, char *argv[])
 	   printf("\nSory but key size is not multiple of block size, try again. TA.\n");
 	   exit(1);
 	}
-	
-
-	f = fopen("/dev/sdb", "r");
-	if(f == NULL) exit(EXIT_FAILURE);
 
 	/* Seek till the end to get disk size and position to start */
-	fseek(f, 0, SEEK_END);
+	fseek(device, 0, SEEK_END);
 
 	/* Determine where is the end */
-	long endOfDisk = ftell(f);
+	long endOfDisk = ftell(device);
 
 	/* Make sure we start again */
-	rewind(f); // Do I need it ???
+	rewind(device); // Do I need it ???
 
 	/* Get the required amount minus block size */
 	long startFrom = endOfDisk - blockSize - keySize;
@@ -46,8 +68,8 @@ void main(int argc, char *argv[])
 	block = calloc(keySize, sizeof(char));
 
 	/* Start reading from specified block */
-	fseek(f, startFrom, SEEK_SET);
-	fread(block, 1, keySize, f);
+	fseek(device, startFrom, SEEK_SET);
+	fread(block, 1, keySize, device);
 
 	/* Do something with the data */
 	for(count = 0; count < keySize/*sizeof(block)*/; count++){
@@ -55,10 +77,9 @@ void main(int argc, char *argv[])
 	}
 
 	/* Close file */
-	fclose(f);
+	fclose(device);
 
 	/* Make sure freed array is zeroed */
 	memset(block, 0, keySize);
 	free(block);
 }
-
